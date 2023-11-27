a841cc0a8a06b1c72b8ff8f253121b3a4551c018
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-11-27 17:40:34 +0100

    Navigation is responsive

--- src/components/Top_Navbar.jsx
+++ src/components/Top_Navbar.jsx
@@ -1,12 +1,85 @@
 import { Col, Container, Navbar, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Top_Navbar = () => {
+  const location = useLocation();
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Row className="flex-grow-1">
-          <Col xs={1} lg={4} className="p-1  align-items-center d-none d-md-flex"></Col>
+    <Navbar expand="md" className="h-100 z-5 bg-body-tertiary px-3">
+      <Container fluid="xl" className="h-100 rounded-start-pill nav-shadow">
+        <Row className="flex-grow-1 h-100">
+          <Col lg={2} className="pe-1 align-items-center justify-content-center d-md-flex me-auto my-2 my-lg-0">
+            <Col className="d-flex justify-content-md-center align-items-center">
+              <Link to="/" className="text-decoration-none text-dark d-flex align-items-center justify-content-center">
+                <div className="d-inline-block rounded-droplet bg-danger me-2"></div>
+                <h2 className="d-inline-block fs-5 m-0">Blood Deft</h2>
+              </Link>
+              <Col className="h-100 d-flex d-md-none align-items-center justify-content-end">
+                <Link
+                  to="/"
+                  className="h-100 mx-3 text-decoration-none  text-dark d-flex align-items-center justify-content-center"
+                >
+                  <span className={`${location.pathname.includes("/") ? "active" : ""}`}>Sign Up</span>
+                </Link>
+                <Link
+                  to="/"
+                  className="h-100 text-decoration-none text-dark d-flex align-items-center justify-content-center"
+                >
+                  <span className={`${location.pathname.includes("/") ? "active" : ""}`}>Log In</span>
+                </Link>
+              </Col>
+            </Col>
+          </Col>
+          <Col md={12} lg={10} className="h-100 align-items-center d-none d-md-flex mb-md-2">
+            <Col className="h-100">
+              <Link
+                to="/"
+                className="h-100 text-decoration-none text-dark d-flex align-items-center justify-content-center "
+              >
+                <span className={`${location.pathname.includes("/") ? "active" : ""}`}>Home</span>
+              </Link>
+            </Col>
+            <Col md={3} className="h-100">
+              <Link
+                to="/"
+                className="h-100 text-decoration-none text-dark d-flex  align-items-center justify-content-center"
+              >
+                <span className={`${location.pathname.includes("/") ? "active" : ""}`}>Make an appointment</span>
+              </Link>
+            </Col>
+            <Col className="h-100">
+              <Link
+                to="/"
+                className="h-100 text-decoration-none text-dark d-flex align-items-center justify-content-center"
+              >
+                <span className={`${location.pathname.includes("/") ? "active" : ""}`}>About</span>
+              </Link>
+            </Col>
+            <Col className="h-100">
+              <Link
+                to="/"
+                className="h-100 text-decoration-none text-dark d-flex align-items-center justify-content-center"
+              >
+                <span className={`${location.pathname.includes("/") ? "active" : ""}`}>Profile</span>
+              </Link>
+            </Col>
+            <Col className="h-100">
+              <Link
+                to="/"
+                className="h-100 text-decoration-none text-dark d-flex align-items-center justify-content-center"
+              >
+                <span className={`${location.pathname.includes("/") ? "active" : ""}`}>Sign Up</span>
+              </Link>
+            </Col>
+            <Col className="h-100">
+              <Link
+                to="/"
+                className="h-100 text-decoration-none text-dark d-flex align-items-center justify-content-center"
+              >
+                <span className={`${location.pathname.includes("/") ? "active" : ""}`}>Log In</span>
+              </Link>
+            </Col>
+          </Col>
+          <Col className="d-md-none"></Col>
         </Row>
       </Container>
     </Navbar>
