74f2a76e7153e42dd58626c6af8e231af8b0efa5
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-11-29 19:32:03 +0100

    Added donations steps section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -30,9 +30,9 @@ const Home = () => {
             </Col>
           </Row>
         </section>
-        <section className="d-none d-md-block custom-section mt-5">
-          <Row className="align-items-center justify-content-md-between">
-            <Col md={10} lg={9} className="d-flex align-items-center text-white py-3 mt-4">
+        <section className="d-none d-md-block custom-section mt-5 mb-5">
+          <Row>
+            <Col md={10} lg={9} className="d-flex align-items-center justify-content-center text-white py-3 mt-4">
               <div>
                 <h2 className="fs-2 mb-3">We are helping pepole all around the world</h2>
                 <p>
@@ -54,8 +54,58 @@ const Home = () => {
             </Col>
           </Row>
         </section>
+        <section className="pt-5 d-none d-md-block">
+          <div className="mb-5">
+            <h2 className="fw-bold fs-2">Donation process</h2>
+            <p className="fs-6 mt-3">Follow these simple steps and help us give hope to those in need.</p>
+          </div>
+          <Row className="row-cols-1 row-cols-sm-2 row-cols-md-4 pt-5">
+            <Col className="step position-relative mb-5">
+              <h3 className="fw-bold fs-3 mb-3">Registration</h3>
+              <p className="custom-fs-6">
+                If you don't have an account yet, click on
+                <Link to="/" className="fw-bold text-decoration-none text-black cursor-pointer">
+                  &nbsp;Sign Up&nbsp;
+                </Link>
+                and fill out the form; otherwise, click
+                <Link to="/" className="fw-bold text-decoration-none text-black cursor-pointer">
+                  &nbsp;Log In&nbsp;
+                </Link>
+                and access your account.
+              </p>
+            </Col>
+            <Col className="step position-relative mb-5">
+              <h3 className="fw-bold fs-3 mb-3">Screening</h3>
+              <p className="custom-fs-6">
+                If this is your first donation at our centers, fill out the modal by clicking
+                <Link to="/" className="fw-bold text-decoration-none text-black cursor-pointer">
+                  &nbsp;here
+                </Link>
+                .
+              </p>
+            </Col>
+            <Col className="step position-relative mb-5">
+              <h3 className="fw-bold fs-3 mb-3">Donation</h3>
+              <p className="custom-fs-6">
+                Book an appointment at one of our locations easily and quickly by clicking
+                <Link to="/" className="fw-bold text-decoration-none text-black cursor-pointer">
+                  &nbsp;here
+                </Link>
+                .
+              </p>
+            </Col>
+            <Col className="step position-relative mb-5">
+              <h3 className="fw-bold fs-3 mb-3">Refreshment</h3>
+              <p className="custom-fs-6">
+                After the donation, a period of rest follows. We will contact you via email as soon as your body is
+                ready (approximately 3 months).
+              </p>
+            </Col>
+          </Row>
+        </section>
       </Container>
-      <section className="d-block d-md-none custom-section mt-5 px-4">
+      {/* MOBILE ONLY */}
+      <section className="d-block d-md-none custom-section mt-5 mb-5 px-4">
         <Row className="align-items-center justify-content-md-between">
           <Col sm={12} className="d-flex align-items-center text-white py-3 mt-4">
             <div>
@@ -79,6 +129,57 @@ const Home = () => {
           </Col>
         </Row>
       </section>
+      <Container className="d-block d-md-none">
+        <section className="pt-5 px-3">
+          <div>
+            <h2 className="fw-bold fs-2">Donation process</h2>
+            <p className="fs-6">Follow these simple steps and help us give hope to those in need.</p>
+          </div>
+          <Row className="row-cols-1 row-cols-sm-2 row-cols-md-4 pt-5">
+            <Col className="step position-relative mt-5 mb-5">
+              <h3 className="fw-bold fs-3 mb-2">Registration</h3>
+              <p className="fs-6">
+                If you don't have an account yet, click on
+                <Link to="/" className="fw-bold text-decoration-none text-black cursor-pointer">
+                  &nbsp;Sign Up&nbsp;
+                </Link>
+                and fill out the form; otherwise, click
+                <Link to="/" className="fw-bold text-decoration-none text-black cursor-pointer">
+                  &nbsp;Log In&nbsp;
+                </Link>
+                and access your account.
+              </p>
+            </Col>
+            <Col className="step position-relative mt-5 mb-5">
+              <h3 className="fw-bold fs-3 mb-2">Screening</h3>
+              <p className="fs-6">
+                If this is your first donation at our centers, fill out the modal by clicking
+                <Link to="/" className="fw-bold text-decoration-none text-black cursor-pointer">
+                  &nbsp;here
+                </Link>
+                .
+              </p>
+            </Col>
+            <Col className="step position-relative mt-5 mb-5">
+              <h3 className="fw-bold fs-3 mb-2">Donation</h3>
+              <p className="fs-6">
+                Book an appointment at one of our locations easily and quickly by clicking
+                <Link to="/" className="fw-bold text-decoration-none text-black cursor-pointer">
+                  &nbsp;here
+                </Link>
+                .
+              </p>
+            </Col>
+            <Col className="step position-relative mt-5 mb-5">
+              <h3 className="fw-bold fs-3 mb-2">Refreshment</h3>
+              <p className="fs-6">
+                After the donation, a period of rest follows. We will contact you via email as soon as your body is
+                ready (approximately 3 months).
+              </p>
+            </Col>
+          </Row>
+        </section>
+      </Container>
     </main>
   );
 };
